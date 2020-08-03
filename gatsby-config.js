966b3d457d31eef5ea1c6a305c99deacebf45d74
Manatsun <69100546+Manatsun@users.noreply.github.com>
2020-08-03 20:11:49 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter Hanson`,
+    title: `Gatsby Default Starter Handson`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
